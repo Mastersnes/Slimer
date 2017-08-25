a7813f843b3315cd9c141ccd64ad941ff04343d6
Steven Nesztler <steven.nesztler@ac-nancy-metz.fr>
2017-08-25 18:04:24 +0200

    Ajout cinematique + difficulté

---
+++ WebContent/app/view/game/cinematiqueView.js
@@ -0,0 +1,70 @@
+/*global define */
+define(["jquery",
+        'underscore',
+        "app/utils/utils"],
+function($, _, Utils) {
+	'use strict';
+
+	return function() {
+		this.init = function() {
+			this.el = "#cinematique";
+			this.max = 8;
+			this.current = 0;
+			this.portrait = $("#portrait").is(":visible");
+			this.makeEvents();
+		};
+
+		this.load = function() {
+			$(this.el).fadeIn();
+			this.current = 1;
+			this.show();
+		};
+		
+		this.next = function() {
+			this.current++;
+			console.log("next : ", this.current);
+			if (this.isEmpty()) {
+				$(this.el).fadeOut("slow");
+			}else {
+				this.show();
+			}
+		};
+		
+		this.show = function() {
+		    var currentCase = $(this.el).find(".case#c"+this.current);
+		    //Si on est en mode portrait, on affiche en ligne
+		    if (this.portrait) {
+		        currentCase.show();
+		    }else {
+		        currentCase.addClass("visible");
+		    }
+		    
+		    if (this.current > 3) {
+                $(this.el).find(".case#c1").hide();
+                $(this.el).find(".case#c2").hide();
+                $(this.el).find(".case#c3").hide();
+            }
+		    if (this.current == 6) {
+		        currentCase.addClass("drama");
+		    }
+            if (this.current > 6) {
+                $(this.el).find(".case#c4").hide();
+                $(this.el).find(".case#c5").hide();
+                $(this.el).find(".case#c6").hide();
+            }
+		};
+		
+		this.makeEvents = function() {
+			var that = this;
+			$(this.el).click(function() {
+				that.next();
+			});
+		};
+		
+		this.isEmpty = function() {
+			return this.current > this.max;
+		};
+		
+		this.init();
+	};
+});
--- WebContent/app/view/game/gameView.js
+++ WebContent/app/view/game/gameView.js
@@ -3,8 +3,9 @@ define(["jquery",
         'underscore',
         "app/utils/utils",
         "text!app/template/game/game.html",
-        "text!app/template/game/end.html"],
-function($, _, Utils, page, endPage) {
+        "text!app/template/game/end.html",
+        "app/view/game/cinematiqueView",],
+function($, _, Utils, page, endPage, CinematiqueView) {
 	'use strict';
 
 	return function(parent, Textes, Mediatheque) {
@@ -16,6 +17,7 @@ function($, _, Utils, page, endPage) {
 			this.point = -1;
 			this.nbrSlime = -1;
 			this.maxSlime = 1000;
+			this.randMax = 100;
 			this.render();
 		};
 
@@ -27,6 +29,9 @@ function($, _, Utils, page, endPage) {
 			};
 			this.el.html(template(templateData));
 			
+			this.cinematique = new CinematiqueView();
+			this.cinematique.load();
+			
 			$("#maxSlime").html(this.maxSlime);
 			this.addPoint();
 			this.addSlime(1);
@@ -36,52 +41,56 @@ function($, _, Utils, page, endPage) {
 		this.loop = function() {
 		    var that = this;
 		    
-		    
-		    var rand = Utils.rand(0, 10);
-		    if (this.firstTime) {
-		        rand = 5;
-		        this.firstTime = false;
-		    }
-		    if(rand == 5) {
-		        var x = Utils.rand(10, 90);
-		        var y = Utils.rand(10, 90);
-		        
-		        if (this.nbrSlime > this.maxSlime) this.gameOver();
-		        
-		        var slime = $("<div></div>")
-		        slime.addClass("slime type"+Utils.rand(1, 7));
-		        slime.attr("life", 3);
-		        slime.css({
-		            left : x + "%",
-		            top : y + "%"
-		        });
-		        
-		        this.addSlime(1);
-		        $(".game").append(slime);
-		        slime.click(function() {
-		            that.mediatheque.playSound("zouip.mp3");
-		            var life = parseInt($(this).attr("life"));
-		            life--;
-		            $(this).addClass("shake");
-		            var sl = $(this);
-		            setTimeout(function() {
-		                sl.removeClass("shake");
-		            }, 500);
-		            if (life <= 0) {
-		                $(this).remove();
-		                that.addPoint();
-		                that.addSlime(-1);
-		            }else {
-		                $(this).attr("life", life);
-		                var x = Utils.rand(10, 90);
-		                var y = Utils.rand(10, 90);
-    		            $(this).css({
-    		                left : x + "%",
-    	                    top : y + "%",
-    		                "transform": "scale("+(life/3)+")"
-    		            });
-		            }
-		        });
+		    if (this.cinematique.isEmpty()) {
+    		    var rand = Utils.rand(0, this.randMax);
+    		    if (this.firstTime) {
+    		        rand = parseInt(this.randMax/2);
+    		        this.firstTime = false;
+    		    }
+    		    console.log("rand : ", rand, this.randMax);
+    		    if(rand == parseInt(this.randMax/2)) {
+    		        var x = Utils.rand(10, 90);
+    		        var y = Utils.rand(10, 90);
+    		        
+    		        if (this.nbrSlime > this.maxSlime) this.gameOver();
+    		        
+    		        var slime = $("<div></div>")
+    		        slime.addClass("slime type"+Utils.rand(1, 7));
+    		        slime.attr("life", 3);
+    		        slime.css({
+    		            left : x + "%",
+    		            top : y + "%"
+    		        });
+    		        
+    		        this.addSlime(1);
+    		        $(".game").append(slime);
+    		        slime.click(function() {
+    		            that.randMax = parseInt(that.randMax/2);
+    		            if (that.randMax<10) that.randMax = 10;
+    		            that.mediatheque.playSound("zouip.mp3");
+    		            var life = parseInt($(this).attr("life"));
+    		            life--;
+    		            $(this).addClass("shake");
+    		            var sl = $(this);
+    		            setTimeout(function() {
+    		                sl.removeClass("shake");
+    		            }, 500);
+    		            if (life <= 0) {
+    		                $(this).remove();
+    		                that.addPoint();
+    		                that.addSlime(-1);
+    		            }else {
+    		                $(this).attr("life", life);
+    		                var x = Utils.rand(10, 90);
+    		                var y = Utils.rand(10, 90);
+        		            $(this).css({
+        		                left : x + "%",
+        	                    top : y + "%",
+        		                "transform": "scale("+(life/3)+")"
+        		            });
+    		            }
+    		        });
+    		    }
 		    }
 		    
 		    setTimeout(function() {
